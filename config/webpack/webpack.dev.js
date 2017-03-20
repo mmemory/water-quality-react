bd7379fc67cbd15aa1be87d203e8b7aa8628fb66
Michael Memory <michaelmemoria@gmail.com>
2017-03-20 14:57:25 -0600

    Added bundle extract to development build

--- config/webpack/webpack.dev.js
+++ config/webpack/webpack.dev.js
@@ -8,6 +8,16 @@ module.exports = merge([
         watch: true
     },
     parts.output({path: PATHS.build}),
+    parts.extractBundles([
+        {
+            name: 'vendor',
+            minChunks: ({resource}) => (
+                resource &&
+                resource.indexOf('node_modules') >= 0 &&
+                resource.match(/\.js$/)
+            )
+        }
+    ]),
     parts.generateSourcemaps({type: 'eval-source-map'}),
     parts.loadSass()
 ]);
