cd9e9c682e6aeef6e1ba351982e4644a73e4bfc6
Michael Memory <michaelmemoria@gmail.com>
2017-03-28 21:55:29 -0600

    Implemented start to header

--- src/js/components/common/Header/Header.js
+++ src/js/components/common/Header/Header.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Header.scss';
+import {Link} from 'react-router-dom';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
     render() {
-
         return (
             <header className="main-header">
-
+                <div className="master-width-wrapper">
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/map">Map</Link></li>
+                    </ul>
+                </div>
             </header>
         );
     };
 }
+
+export default Header;
