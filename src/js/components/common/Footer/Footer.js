39bc2e70a7af3b6439a01cbf4c2194f36dde68d3
Michael Memory <michaelmemoria@gmail.com>
2017-03-28 21:51:58 -0600

    Implemented footer

---
+++ src/js/components/common/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer-main">
+
+        </footer>
+    );
+};
+
+export default Footer;
