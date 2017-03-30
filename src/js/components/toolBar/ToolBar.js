0dcee6a598809ac110952de0914077a23f4d3cce
Michael Memory <michaelmemoria@gmail.com>
2017-03-29 18:48:07 -0600

    Created toolbar

---
+++ src/js/components/toolBar/ToolBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './toolBar.scss';
+
+
+const MenuButton = (props) => {
+
+    return (
+        <div className="menu-button">
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+        </div>
+    );
+};
+
+const ToolBar = (props) => {
+
+
+    return (
+        <div className="toolbar-container">
+            <div className="master-width-wrapper">
+
+                <div className="menu-button-container">
+                    <MenuButton />
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ToolBar;
