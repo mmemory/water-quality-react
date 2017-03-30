d963cc9f129b059db115859d6a8ad47774e74e71
Michael Memory <michaelmemoria@gmail.com>
2017-03-29 18:39:59 -0600

    Added wildcard endpoint for single page requests

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+
 app.listen(config.port, function(err) {
     if (err) return;
     console.log(`Listening on port ${config.port}`);
