c6ae3e8c762a2e7709219260974dd80d15446d16
Michael Memory <michaelmemoria@gmail.com>
2017-03-29 18:49:15 -0600

    Made circle display dynamic and adjusted styling of map container for display

--- src/js/components/map/Map.js
+++ src/js/components/map/Map.js
@@ -2,59 +2,19 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import './map.scss';
 import mapStyle from './map-style.json';
-console.log(mapStyle);
 
-// const InitGoogleMap = withGoogleMap(props => (
-//     <GoogleMap
-//         ref={props.onMapLoad}
-//         defaultZoom={props.zoom}
-//         defaultCenter={{ lat: props.lat, lng: props.lng }}
-//         onClick={props.onMapClick}
-//         center={props.center}
-//         zoom={props.zoom}
-//     >
-//         {console.log(props.circle)}
-//
-//
-//         <Circle
-//             {...props.circle}
-//             onClick={_.noop}
-//             onRightClick={_.noop}
-//             onDragStart={_.noop}
-//         />
-//
-//     </GoogleMap>
-// ));
+const Circle = (props) => {
+    const customClass = props.$hover ? 'map-circle-hover' : 'map-circle';
 
-// {/*<InitGoogleMap*/}
-//     {/*containerElement={<div className="map-container" />}*/}
-//     {/*mapElement={<div className="map-element" />}*/}
-//     {/*lat={this.props.lat}*/}
-//     {/*lng={this.props.lng}*/}
-//     {/*zoom={this.props.zoom}*/}
-//     {/*onMapLoad={_.noop}*/}
-//     {/*onMapClick={_.noop}*/}
-//     {/*circle={circle}*/}
-//     {/*center={center}*/}
-// {/*/>*/}
-
-
-
-class Circle extends React.Component {
-    render() {
-        const customClass = this.props.$hover ? 'map-circle-hover' : 'map-circle';
-
-        return (
-            <div className={customClass}></div>
-        );
-    }
-}
+    return (
+        <div style={props.style} className={customClass}></div>
+    );
+};
 
 class Map extends React.Component {
 
 
     shouldComponentUpdate(nextProps, nextState) {
-        // console.log('MAP',nextProps);
         return true;
     }
 
@@ -80,15 +40,19 @@ class Map extends React.Component {
             zoom: 11
         };
 
+        let circleStyle = this.props.showLoader ? {'display': 'none'} : {'display': 'block'};
+
+
         return (
             <GoogleMapReact
                 defaultCenter={settings.center}
+                bootstrapURLKeys={{key: 'AIzaSyCEnKcbM3rv45LbfeE21QWqgOPERexomXU'}}
                 defaultZoom={settings.zoom}
                 center={settings.center}
                 zoom={settings.zoom}
                 options={this.createMapOptions}>
 
-                <Circle lat={settings.center.lat} lng={settings.center.lng}/>
+                <Circle style={circleStyle} lat={settings.center.lat} lng={settings.center.lng}/>
 
             </GoogleMapReact>
         );
