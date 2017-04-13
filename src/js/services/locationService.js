8284307ee00703557a181b0e4e4fc09a4c42302e
Michael Memory <michaelmemoria@gmail.com>
2017-04-12 20:56:36 -0600

    Cleaned up location service

--- src/js/services/locationService.js
+++ src/js/services/locationService.js
@@ -1,23 +1,38 @@
 import axios from 'axios';
 
 export default {
-    locationByCity: (city) => {
-        return axios.get('https://maps.googleapis.com/maps/api/geocode/json?address="'+encodeURIComponent(city))
-            .then((response) => {
+    locationByCity: city => {
+        return axios.get('https://maps.googleapis.com/maps/api/geocode/json?address="' + encodeURIComponent(city))
+            .then(response => {
                 console.log(response);
                 let res = response.data.results;
-                if(res.length === 1) {
+                if (res.length === 1) {
                     let coords = res[0].geometry.location;
 
-                    let newState = {
+                    return {
                         lat: coords.lat,
                         lng: coords.lng,
                         zoom: 12,
                         showLoader: false
                     };
-
-                    return newState;
                 }
+            });
+    },
+
+    locationByIp: () => {
+        return axios.get('http://ip-api.com/json')
+            .then(location => {
+                console.log(location);
+                return {
+                    lat: location.data.lat,
+                    lng: location.data.lon,
+                    zoom: 12,
+                    showLoader: false,
+                    city: location.data.city,
+                    zip: location.data.zip
+                };
+            }, err => {
+                console.log(err);
             });
     }
 }
