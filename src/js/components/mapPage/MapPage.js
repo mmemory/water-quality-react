9c191ff247210f69712a56f4349e02ac5caa3bc4
Michael Memory <michaelmemoria@gmail.com>
2017-04-12 20:37:37 -0600

    Implemented toolbar, adjusted default settings, improved location services

--- src/js/components/mapPage/MapPage.js
+++ src/js/components/mapPage/MapPage.js
@@ -3,77 +3,103 @@ import Map from './../map/Map';
 import ToolBar from './../toolBar/ToolBar';
 import Loader from './../common/loader/Loader';
 import locationService from './../../services/locationService';
+import waterData from './../../services/waterDataService';
+import classNames from 'classnames';
 
 class MapPage extends React.Component {
     constructor() {
         super();
 
+        // Initial map settings
         this.state = {
-            lat: 0,
-            lng: 0,
-            zoom: 3,
-            showLoader: false
-        }
+            lat: 38,
+            lng: -97,
+            zoom: 4,
+            showLoader: false,
+            toolbarOpen: true,
+            waterData: {
+                activity: []
+            }
+        };
+
+        this.handleToolbarOpen = this.handleToolbarOpen.bind(this);
     }
 
     locationByCity(city) {
         locationService.locationByCity(city)
-            .then((data) => {
+            .then(data => {
+                this.getDataByCurrentLocation({
+                    latitude: data.lat,
+                    longitude: data.lng
+                });
                 this.setState(data);
             });
     }
 
     ipLocation() {
-        axios.get('http://ip-api.com/json')
-            .then((location) => {
-                console.log(location);
-                this.setState({
-                    lat: location.data.lat,
-                    lng: location.data.lon,
-                    zoom: 12,
-                    showLoader: false
-                })
-            }, (err) => {
-                console.log(err);
+        locationService.locationByIp()
+            .then(data => {
+                this.locationByCity(data.city);
             });
     }
 
     navigatorLocation() {
-        navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position);
+        navigator.geolocation.getCurrentPosition(position => {
+            this.getDataByCurrentLocation(position.coords);
             this.setState({
                 lat: position.coords.latitude,
                 lng: position.coords.longitude,
                 zoom: 12,
                 showLoader: false
             });
-        }, (err) => {
+        }, err => {
             console.log(err);
             this.ipLocation();
         });
     }
 
+    getDataByCurrentLocation(coords) {
+        waterData.getDataByCurrentLocation(coords)
+            .then(waterData => {
+                console.log(waterData);
+                this.setState({
+                    waterData: {activity: waterData.Organization.Activity}
+                });
+            });
+    }
+
     componentDidMount() {
-        this.setState({
-            showLoader: true
-        });
+        this.setState({showLoader: true});
 
         if (navigator.geolocation) {
             this.navigatorLocation();
         } else {
             this.ipLocation();
         }
+    }
 
-        // this.locationByCity('Provo')
+    handleToolbarOpen(e) {
+        e.stopPropagation();
+        this.setState({
+            toolbarOpen: !this.state.toolbarOpen
+        });
     }
 
     render() {
         let loaderStyle = this.state.showLoader ? {'display': 'block'} : {'display': 'none'};
 
+        let mapClasses = classNames({
+            'map-container': true,
+            'toolbar-open': this.state.toolbarOpen,
+            'toolbar-closed': !this.state.toolbarOpen
+        });
+
         return (
             <div className="map-wrapper">
-                <ToolBar />
-                <div className="map-container">
+                <ToolBar activity={this.state.waterData.activity}
+                         toolbarOpen={this.state.toolbarOpen}
+                         handleToolbarOpen={this.handleToolbarOpen}/>
+                <div className={mapClasses}>
                     <Map {...this.state} />
                     <Loader style={loaderStyle}/>
                 </div>
--- src/js/components/toolBar/ToolBar.js
+++ src/js/components/toolBar/ToolBar.js
@@ -1,22 +1,56 @@
 import React from 'react';
+import classNames from 'classnames';
+import keyGen from 'random-key';
 
 const MenuButton = (props) => {
 
     return (
-        <div className="menu-button">
-            <div className="bar"></div>
-            <div className="bar"></div>
-            <div className="bar"></div>
-        </div>
+        <div className="menu-button" onClick={props.handleToolbarOpen}></div>
     );
 };
 
 const ToolBar = (props) => {
 
+    let toolbarClasses = classNames({
+        'toolbar-container': true,
+        'toolbar-closed': !props.toolbarOpen,
+        'toolbar-open': props.toolbarOpen
+    });
+
+    let waterData = [];
+
+    props.activity.forEach(function(item) {
+
+        if (item.Result instanceof Array) {
+            waterData = [
+                ...waterData,
+                ...item.Result
+            ];
+        } else if (item.Result.ResultDescription) {
+            waterData.push(item);
+        }
+    });
+
+    waterData = waterData.map(function(d) {
+        if(d && d.ResultDescription && d.ResultDescription.ResultMeasure) {
+            return (
+                <li key={keyGen.generate()}>
+                    <span>{d.ResultDescription.CharacteristicName.text}: </span>
+                    <span>{d.ResultDescription.ResultMeasure.ResultMeasureValue.text} {d.ResultDescription.ResultMeasure.MeasureUnitCode.text}</span>
+                </li>
+            );
+        }
+    });
 
     return (
-        <div className="toolbar-container">
+        <div className={toolbarClasses}>
+            <MenuButton handleToolbarOpen={props.handleToolbarOpen}/>
 
+            <div className="water-data">
+                <ul>
+                    {waterData}
+                </ul>
+            </div>
         </div>
     );
 };
