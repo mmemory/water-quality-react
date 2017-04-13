d07bfa1a2e571a381ae3a0103a913294fe7226ea
Michael Memory <michaelmemoria@gmail.com>
2017-04-12 20:58:42 -0600

    Basic water data functionality

---
+++ src/js/services/waterDataService.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import parseData from './parseDataService';
+
+export default {
+    getDataByCurrentLocation: function(coords, characteristics, date) {
+        return axios.get(`https://www.waterqualitydata.us/Result/search?Zip=yes&lat=${coords.latitude}&long=${coords.longitude}&within=20&startDateLo=${parseData.parseDate(date)}&mimeType=xml&characteristicName=Fluoride;Sulfate;Dissolved solids&siteType=Well;Spring;Aggregate surface-water-use;Aggregate groundwater use;Lake, Reservoir, Impoundment`)
+            .then(function(data) {
+                return parseData.xmlToJson(parseData.parseXml(data.data)).WQX;
+            });
+    }
+}
