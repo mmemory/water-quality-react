cf88ef9646d5ba561056fc71d04da17fc8fb90d0
Michael Memory <michaelmemoria@gmail.com>
2017-04-15 16:59:30 -0600

    Adjusted xml parser to always put organizations into an array

--- src/js/services/parseDataService.js
+++ src/js/services/parseDataService.js
@@ -1,10 +1,10 @@
 let parseHelpers = {
 
     // mm-dd-yyyy
-    parseDate: function(date) {
+    parseDate: function (date) {
         let d = date ? new Date(date) : new Date();
-        d.setMonth(d.getMonth() - 6);
-        d.setHours(0,0,0);
+        d.setMonth(d.getMonth() - 12);
+        d.setHours(0, 0, 0);
 
         let twoDigitDate = d.getDate() < 10 ? `0${d.getDate()}` : d.getDate();
         let twoDigitMonth = d.getMonth() < 10 ? `0${d.getMonth()}` : d.getMonth();
@@ -31,14 +31,24 @@ let parseHelpers = {
 
         // do children
         if (xml.hasChildNodes()) {
-            for(let i = 0; i < xml.childNodes.length; i++) {
+            for (let i = 0; i < xml.childNodes.length; i++) {
+                //debugger;
                 let item = xml.childNodes.item(i);
                 let nodeName = item.nodeName;
-                if(nodeName.charAt(0) === '#') nodeName = nodeName.split('').splice(1).join('');
-                if (typeof(obj[nodeName]) == "undefined") {
+                if (nodeName.charAt(0) === '#') nodeName = nodeName.split('').splice(1).join('');
+                if (item.parentNode.nodeName === '#document') {
                     obj[nodeName] = parseHelpers.xmlToJson(item);
+                } else if (typeof(obj[nodeName]) === "undefined") {
+                    /* Possible to have more than one organization. The following logic puts the organization object in an array
+                    even if there is only one organization. Makes it more predictable to sift through the data
+                    knowing that there will always be an array */
+                    if(item.nodeName === "Organization" && typeof(item.push) !== "undefined") {
+                        obj[nodeName] = [parseHelpers.xmlToJson(item)];
+                    } else {
+                        obj[nodeName] = parseHelpers.xmlToJson(item);
+                    }
                 } else {
-                    if (typeof(obj[nodeName].push) == "undefined") {
+                    if (typeof(obj[nodeName].push) === "undefined") {
                         let old = obj[nodeName];
                         obj[nodeName] = [];
                         obj[nodeName].push(old);
@@ -49,19 +59,16 @@ let parseHelpers = {
         }
         return obj;
     },
-
-
 };
-
 
 // Set up correct xml parser
 if (typeof window.DOMParser != "undefined") {
-    parseHelpers.parseXml = function(xmlStr) {
+    parseHelpers.parseXml = function (xmlStr) {
         return ( new window.DOMParser() ).parseFromString(xmlStr, "text/xml");
     };
 } else if (typeof window.ActiveXObject != "undefined" &&
     new window.ActiveXObject("Microsoft.XMLDOM")) {
-    parseHelpers.parseXml = function(xmlStr) {
+    parseHelpers.parseXml = function (xmlStr) {
         let xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
         xmlDoc.async = "false";
         xmlDoc.loadXML(xmlStr);
