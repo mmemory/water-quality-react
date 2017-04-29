ec1c7f3cac8940ce8beffb767b9c0a0460732dc2
Michael Memory <michaelmemoria@gmail.com>
2017-04-29 12:02:04 -0600

    Separated homepage pieces into components

--- src/js/components/homePage/Home.js
+++ src/js/components/homePage/Home.js
@@ -2,41 +2,21 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import Footer from './../common/Footer/Footer';
 import ShowcaseContainer from './showcaseContainer/showcaseContainer';
+import HeroContainer from './heroContainer/heroContainer';
+import ActionCallSection from './actionCallSection/actionCallSection';
+import MobileShowcaseContainer from './mobileShowcaseContainer/mobileShowcaseContainer';
 
 
 const Home = () => {
     return (
         <div className="content-main">
-            <section className="hero-container">
-                <div className="master-width-wrapper">
-
-                    <div className="title-container">
-                        <h2 className="hero-title">Know what you drink</h2>
-                        <Link to="/map">
-                            <div className="ghost-btn btn-small">
-                                Sign Up
-                            </div>
-                        </Link>
-                    </div>
-
-                </div>
-            </section>
+            <HeroContainer/>
             <main>
                 <ShowcaseContainer/>
 
-                <section className="action-call">
-                    <Link to="/map">
-                        <div className="ghost-btn btn-medium">
-                            See For Yourself
-                        </div>
-                    </Link>
-                </section>
-                <section className="mobile-showcase-container">
-                    <div className="master-width-wrapper">
+                <ActionCallSection/>
 
-
-                    </div>
-                </section>
+                <MobileShowcaseContainer/>
             </main>
             <Footer />
         </div>
